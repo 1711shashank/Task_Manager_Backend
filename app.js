2a37aafe67167109f308cff9aba9991912c1aa25
Dipikesh <dipikesh.singh.915@gmail.com>
2023-02-28 23:44:19 +0530

    auth added

--- app.js
+++ app.js
@@ -1,165 +1,164 @@
 const express = require("express");
-const { all } = require("express/lib/application");
-const { timeSheetDataBase, taskSheetDataBase } = require('./mongoDB');
+const { timeSheetDataBase, taskSheetDataBase } = require("./mongoDB");
+const { oauth ,validateCallback} = require("./auth");
 const app = express();
-
-var cors = require('cors');
+var cors = require("cors");
 
 app.use(cors({ origin: true, optionsSuccessStatus: 200, credentials: true }));
-app.options( "*", cors({ origin: true, optionsSuccessStatus: 200, credentials: true }) );
+app.options(
+  "*",
+  cors({ origin: true, optionsSuccessStatus: 200, credentials: true })
+);
 
 app.use(express.json());
 
-const port = 5000
+const port = 5000;
 app.listen(port);
 
-app.get('/getTask', getTask);
-app.get('/getSubTask', getSubTask);
-app.get('/getActivity', getActivity);
+app.get("/getTask", getTask);
+app.get("/getSubTask", getSubTask);
+app.get("/getActivity", getActivity);
 
-app.post('/addTask', addTask);
-app.post('/addSubTask', addSubTask);
-app.post('/addActivity', addActivity);
+app.post("/addTask", addTask);
+app.post("/addSubTask", addSubTask);
+app.post("/addActivity", addActivity);
 
-app.post('/deleteTask', deleteTask);
-app.post('/deleteSubTask', deleteSubTask);
-app.post('/deleteActivity', deleteActivity);
+app.post("/deleteTask", deleteTask);
+app.post("/deleteSubTask", deleteSubTask);
+app.post("/deleteActivity", deleteActivity);
+
+app.get("/oauth", oauth);
+app.get("/validate-callback", validateCallback);
 
 
-async function getActivity(req,res){
-    let TimeSheetData = await timeSheetDataBase.find();
-    res.status(200).json({
-        TimeSheetData
-    })
+
+async function getActivity(req, res) {
+  let TimeSheetData = await timeSheetDataBase.find();
+  res.status(200).json({
+    TimeSheetData,
+  });
 }
 
 async function addActivity(req, res) {
-    try {
-        const { Date, Activity } = req.body.newEntry;
+  try {
+    const { Date, Activity } = req.body.newEntry;
 
-        await timeSheetDataBase.findOneAndUpdate(
-            { Date: Date },                            // filter
-            { $addToSet: { Activity: Activity } },        // update
-            { upsert: true, new: true }                    // conduction
-        );
-        res.status(200).json({
-            Message: "Activity Added"
-        })
-
-    } catch (err) {
-        console.log(err);
-    }
-
-    // $pull in update => 2nd varible for delete
-}
-async function deleteActivity(req, res){
-    try {
-        const { _id, id } = req.body;
-        
-        await timeSheetDataBase.findOneAndUpdate(
-            { 'Activity.id': id, _id:_id },             // filter
-            { $pull: { Activity:{id:id} } },             // update
-            { upsert: true, new: true }                  // conduction
-        );
-
-        res.status(200).json({
-            Message: "Activity Deleted"
-        })
-
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-async function getTask(req,res){
-    let TaskSheetData = await taskSheetDataBase.find();
+    await timeSheetDataBase.findOneAndUpdate(
+      { Date: Date }, // filter
+      { $addToSet: { Activity: Activity } }, // update
+      { upsert: true, new: true } // conduction
+    );
     res.status(200).json({
-        TaskSheetData
-    })
+      Message: "Activity Added",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  // $pull in update => 2nd varible for delete
+}
+async function deleteActivity(req, res) {
+  try {
+    const { _id, id } = req.body;
+
+    await timeSheetDataBase.findOneAndUpdate(
+      { "Activity.id": id, _id: _id }, // filter
+      { $pull: { Activity: { id: id } } }, // update
+      { upsert: true, new: true } // conduction
+    );
+
+    res.status(200).json({
+      Message: "Activity Deleted",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getTask(req, res) {
+  let TaskSheetData = await taskSheetDataBase.find();
+  res.status(200).json({
+    TaskSheetData,
+  });
 }
 
 async function addTask(req, res) {
-    try {
-        const { TaskName, SubTasks } = req.body.newEntry;
+  try {
+    const { TaskName, SubTasks } = req.body.newEntry;
 
-        const newTask = new taskSheetDataBase({
-            TaskName:TaskName, SubTasks:SubTasks
-        });
+    const newTask = new taskSheetDataBase({
+      TaskName: TaskName,
+      SubTasks: SubTasks,
+    });
 
-        await newTask.save();
+    await newTask.save();
 
-        res.status(200).json({
-            Message: "Task Added"
-        })
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-async function deleteTask(req, res){
-    try {
-        const { _id } = req.body;
-
-        let taskToBeDeleted = await taskSheetDataBase.findOne({ _id: _id });
-        if (taskToBeDeleted) {
-            await taskSheetDataBase.deleteOne(taskToBeDeleted['_id']);
-        }
-        
-        res.status(200).json({
-            Message: "Task Deleted"
-        })
-
-    } catch (err) {
-        console.log(err);
-    }
-
-}
-
-async function getSubTask(req,res){
-    let TaskSheetData = await taskSheetDataBase.find();
     res.status(200).json({
-        TaskSheetData
-    })
+      Message: "Task Added",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function addSubTask(req, res){
+async function deleteTask(req, res) {
+  try {
+    const { _id } = req.body;
 
-    try {
-        const {TaskId, SubTask} = req.body;
-        console.log(req.body);
-
-        await taskSheetDataBase.findOneAndUpdate(
-            { _id: TaskId },                                     // filter
-            { $addToSet: { SubTasks: SubTask } },               // update
-            { upsert: true, new: true }                        // conduction
-        );
-        
-        res.status(200).json({
-            Message: "Sub Task Added"
-        })
-
-        
-    } catch (err) {
-        console.log(err);
+    let taskToBeDeleted = await taskSheetDataBase.findOne({ _id: _id });
+    if (taskToBeDeleted) {
+      await taskSheetDataBase.deleteOne(taskToBeDeleted["_id"]);
     }
+
+    res.status(200).json({
+      Message: "Task Deleted",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function deleteSubTask(req, res){
-    try {
-        const { TaskId, SubTaskId } = req.body.subTaskToBeDeleted;
-        console.log("delete",req.body);
+async function getSubTask(req, res) {
+  let TaskSheetData = await taskSheetDataBase.find();
+  res.status(200).json({
+    TaskSheetData,
+  });
+}
 
-        await taskSheetDataBase.findOneAndUpdate(
-            { 'SubTasks.SubTaskId': SubTaskId, _id:TaskId },                            
-            { $pull: {SubTasks:{SubTaskId: SubTaskId }}},                    
-            { upsert: true, new: true }                   
-        )
+async function addSubTask(req, res) {
+  try {
+    const { TaskId, SubTask } = req.body;
+    console.log(req.body);
 
-        res.status(200).json({
-            Message: "Sub Task Deleted"
-        })
+    await taskSheetDataBase.findOneAndUpdate(
+      { _id: TaskId }, // filter
+      { $addToSet: { SubTasks: SubTask } }, // update
+      { upsert: true, new: true } // conduction
+    );
 
-    } catch (err) {
-        console.log(err);
-    }
+    res.status(200).json({
+      Message: "Sub Task Added",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function deleteSubTask(req, res) {
+  try {
+    const { TaskId, SubTaskId } = req.body.subTaskToBeDeleted;
+    console.log("delete", req.body);
+
+    await taskSheetDataBase.findOneAndUpdate(
+      { "SubTasks.SubTaskId": SubTaskId, _id: TaskId },
+      { $pull: { SubTasks: { SubTaskId: SubTaskId } } },
+      { upsert: true, new: true }
+    );
+
+    res.status(200).json({
+      Message: "Sub Task Deleted",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
